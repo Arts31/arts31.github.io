a739dc81b286638303b512ce365ea7ba8d805865
arts31 <38892294+arts31@users.noreply.github.com>
2018-07-22 10:42:04 +0300

    v 8.0 and v 5.0

--- order.js
+++ order.js
@@ -1,44 +1,43 @@
 function updateorder() {
-	const one_1 = 790;
-	const four_1 = 2500;
-	const one_2 = 1600;
-	const three_2 = 2600;
-	const one_4 = 9000;
+	const one_3 = 550;
+	const one_5 = 500;
+	const two_1 = 620;
+	const two_4 = 300;
+	const five_2 = 500;
 	const pizza = 1200;
-	var num_one_1 =
-	document.getElementById('1/1').value;
-	var num_four_1 =
-	document.getElementById('4/1').value;
-	var num_one_2 =
-	document.getElementById('1/2').value;
-	var num_three_2 =
-	document.getElementById('3/2').value;
-	var num_one_4 =
-	document.getElementById('1/4').value;
+	var num_one_3 =
+	document.getElementById('1/3').value;
+	var num_one_5 =
+	document.getElementById('1/5').value;
+	var num_two_1 =
+	document.getElementById('2/1').value;
+	var num_two_4 =
+	document.getElementById('2/4').value;
+	var num_five_2 =
+	document.getElementById('5/2').value;
 	var num_pizza =
 	document.getElementById('pizza').value
-	if (isNaN(num_one_1)) {
-		num_one_1 = 0;
+	if (isNaN(num_one_3)) {
+		num_one_3 = 0;
 	}
-	if (isNaN(num_four_1)) {
-		num_four_1 = 0;
+	if (isNaN(num_one_5)) {
+		num_one_5 = 0;
 	}
-	if (isNaN(num_one_2)) {
-		num_one_2 = 0;
+	if (isNaN(num_two_1)) {
+		num_two_1 = 0;
 	}
-	if (isNaN(num_three_2)) {
-		num_three_2 = 0;
+	if (isNaN(num_two_4)) {
+		num_two_4 = 0;
 	}
-	if (isNaN(num_one_4)) {
-		num_one_4 = 0;
+	if (isNaN(num_five_2)) {
+		num_five_2 = 0;
 	}
 	if (isNaN(pizza)) {
 		num_pizza = 0;
 	}
-	var total = num_one_1 * one_1 + num_one_2 * one_2 +
-	num_three_2 * three_2 + num_one_4 * one_4 + num_pizza *
-	pizza + num_four_1 * four_1;
-	document.getElementById('total').value = "Р" + total;
+	var total = num_one_3 * one_3 + num_one_5 * one_5 + num_two_1 * two_1 + num_two_4
+	* two_4 + num_five_2 * five_2 + num_pizza;
+	document.getElementById('total').value = "₽ " + total;
 
 
 }
